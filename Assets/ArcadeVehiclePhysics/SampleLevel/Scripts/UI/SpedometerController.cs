08627e1301ab4ee0942f4a80c8d36bd2bce79923
leminhduc201299 <duc.lm173043@sis.hust.edu.vn>
2021-04-19 20:44:52 +0700

    Init

---
+++ Assets/ArcadeVehiclePhysics/SampleLevel/Scripts/UI/SpedometerController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class SpedometerController : MonoBehaviour
+{
+    public TextMeshProUGUI forwardSpeedText;
+    public Image forwardSpeedBar;
+    public TextMeshProUGUI sideSpeedText;
+    public Image sideSpeedBar;
+    public TextMeshProUGUI velMagText;
+    public Image velMagBar;
+    //public TextMeshProUGUI vertText;
+    //public Image vertBar;
+
+    public CpMain cpMain;
+    private VehicleSpeed _speedData;
+
+    private CaBoost CaBoost;
+    public Image BoostBar;
+    
+    private CaJump CaJump;
+    public Image JumpCharge;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (cpMain == null)
+        {
+            cpMain = FindObjectOfType<CpMain>();
+        }
+
+        CaBoost = cpMain.GetComponent<CaBoost>();
+        CaJump = cpMain.GetComponent<CaJump>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _speedData = cpMain.speedData;
+
+        float speedPercentage = (_speedData.ForwardSpeedPercent);
+        UpdateSpeedBar(speedPercentage, forwardSpeedBar, forwardSpeedText,900);
+        
+        float velMagPercentage = new Vector2(_speedData.forwardSpeed, _speedData.sideSpeed).magnitude / _speedData.topSpeed;
+        UpdateSpeedBar(velMagPercentage, velMagBar, velMagText,910);
+        velMagText.rectTransform.localPosition += Vector3.up * 50;
+        
+        float sideSpeedPercentage = _speedData.SideSpeedPercent;
+        UpdateSpeedBar(sideSpeedPercentage, sideSpeedBar, sideSpeedText, 900);
+
+        
+        if (CaBoost!=null)
+        {
+            BoostBar.rectTransform.sizeDelta = new Vector2(BoostBar.rectTransform.sizeDelta.x, 900 * (CaBoost.currentBoostTimeLeft/CaBoost.boostTimeMax));
+        }
+        
+        if (CaJump!=null)
+        {
+            JumpCharge.rectTransform.sizeDelta = new Vector2(JumpCharge.rectTransform.sizeDelta.x, 900 * (CaJump.currentCharge/CaJump.forceMax));
+        }
+    }
+
+    private void UpdateSpeedBar(float valuePercentage, Image bar, TextMeshProUGUI text, int imageSize)
+    {
+        text.text = ((int)(valuePercentage * _speedData.topSpeed)).ToString();
+
+        valuePercentage = Mathf.Clamp01(valuePercentage);
+        text.rectTransform.localPosition = new Vector3(text.rectTransform.localPosition.x, -imageSize/2 + valuePercentage * imageSize, text.rectTransform.localPosition.z);
+        bar.rectTransform.sizeDelta = new Vector2(bar.rectTransform.sizeDelta.x, imageSize * valuePercentage);
+    }
+    
+}
---
+++ Assets/ArcadeVehiclePhysics/Vehicle/Scripts/CP - CarPhysics/CpDrag.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CpDrag : MonoBehaviour
+{
+    [Header("Drag")]
+    public float linearDrag;
+    public float freeWheelDrag;
+    public float brakingDrag;
+    public float angularDrag;
+
+    public bool linearDragCheck;
+    public bool brakingDragCheck;
+    public bool freeWheelDragCheck;
+
+    private CpMain _cpMain;
+
+    private void Awake()
+    {
+        _cpMain = transform.parent.GetComponent<CpMain>();
+
+        _cpMain.rb.angularDrag = angularDrag;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        UpdateDrag(
+            _cpMain.rb,
+            _cpMain.wheelData.grounded,
+            _cpMain.input,
+            _cpMain.speedData
+        );
+
+    }
+
+    private void UpdateDrag(Rigidbody rb, bool grounded, PlayerInputs input, VehicleSpeed speedData)
+    {
+        linearDragCheck = Mathf.Abs(input.accelInput) < 0.05 || grounded;
+        float linearDragToAdd = linearDragCheck ? linearDrag : 0;
+
+        brakingDragCheck = input.accelInput < 0 && speedData.forwardSpeed > 0;
+        float brakingDragToAdd = brakingDragCheck ? brakingDrag : 0;
+        
+        freeWheelDragCheck = Math.Abs(input.accelInput) < 0.02f && grounded;
+        float freeWheelDragToAdd = freeWheelDragCheck ? freeWheelDrag : 0;
+
+        rb.drag = linearDragToAdd + brakingDragToAdd + freeWheelDragToAdd;
+    }
+}
