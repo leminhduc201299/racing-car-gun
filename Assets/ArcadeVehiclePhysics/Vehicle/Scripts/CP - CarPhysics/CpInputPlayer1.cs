9a292dc388b93ea3bf754ded3a48d15afc96ee35
leminhduc201299 <duc.lm173043@sis.hust.edu.vn>
2021-04-21 21:32:13 +0700

    fix bug cho huy

--- Assets/ArcadeVehiclePhysics/Vehicle/Scripts/CP - CarPhysics/CpInputPlayer1.cs
+++ Assets/ArcadeVehiclePhysics/Vehicle/Scripts/CP - CarPhysics/CpInputPlayer1.cs
@@ -4,34 +4,33 @@ using UnityEngine;
 
 public class CpInputPlayer1 : CpInput
 {
-  // Update is called once per frame
-  void Update()
-  {
-    HandleInputs();
-  }
-
-  private void HandleInputs()
-  {
-    //Forward/Reverse
-    if (
-      Input.GetKeyDown(KeyCode.W) ||
-      Input.GetKeyDown(KeyCode.S) ||
-      Input.GetKeyUp(KeyCode.W) ||
-      Input.GetKeyUp(KeyCode.S)
-      )
+    public float speed = 10;
+    // Update is called once per frame
+    void Update()
     {
-      _cpMain.input.accelInput = Input.GetAxis("Vertical");
+        HandleInputs();
     }
 
-    //Steering
-    if (
-      Input.GetKeyDown(KeyCode.A) ||
-      Input.GetKeyDown(KeyCode.D) ||
-      Input.GetKeyUp(KeyCode.A) ||
-      Input.GetKeyUp(KeyCode.D)
-    )
+    private void HandleInputs()
     {
-      _cpMain.input.steeringInput = Input.GetAxis("Horizontal");
+        //Forward/Reverse
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S))
+        {
+            _cpMain.input.accelInput = Input.GetAxis("Vertical") * speed;
+        }
+        else if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S))
+        {
+            _cpMain.input.accelInput = 0;
+        }
+
+        //Steering
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+        {
+            _cpMain.input.steeringInput = Input.GetAxis("Horizontal") * speed;
+        }
+        else if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        {
+            _cpMain.input.steeringInput = 0;
+        }
     }
-  }
 }
--- Assets/ArcadeVehiclePhysics/Vehicle/Scripts/CP - CarPhysics/CpInputPlayer2.cs
+++ Assets/ArcadeVehiclePhysics/Vehicle/Scripts/CP - CarPhysics/CpInputPlayer2.cs
@@ -4,34 +4,34 @@ using UnityEngine;
 
 public class CpInputPlayer2 : CpInput
 {
-  // Update is called once per frame
-  void Update()
-  {
-    HandleInputs();
-  }
+    public float speed = 10;
 
-  private void HandleInputs()
-  {
-    //Forward/Reverse
-    if (
-      Input.GetKeyDown(KeyCode.DownArrow) ||
-      Input.GetKeyDown(KeyCode.UpArrow) ||
-      Input.GetKeyUp(KeyCode.DownArrow) ||
-      Input.GetKeyUp(KeyCode.UpArrow)
-      )
+    // Update is called once per frame
+    void Update()
     {
-      _cpMain.input.accelInput = Input.GetAxis("Vertical");
+        HandleInputs();
     }
 
-    //Steering
-    if (
-      Input.GetKeyDown(KeyCode.RightArrow) ||
-      Input.GetKeyDown(KeyCode.LeftArrow) ||
-      Input.GetKeyUp(KeyCode.RightArrow) ||
-      Input.GetKeyUp(KeyCode.LeftArrow)
-    )
+    private void HandleInputs()
     {
-      _cpMain.input.steeringInput = Input.GetAxis("Horizontal");
+        //Forward/Reverse
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            _cpMain.input.accelInput = Input.GetAxis("Vertical") * speed;
+        }
+        else if (Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            _cpMain.input.accelInput = 0;
+        }
+
+        //Steering
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            _cpMain.input.steeringInput = Input.GetAxis("Horizontal") * speed;
+        }
+        else if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            _cpMain.input.steeringInput = 0;
+        }
     }
-  }
 }
